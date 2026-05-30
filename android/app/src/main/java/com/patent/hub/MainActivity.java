9e89010c07fb269bf249a94146702c5d66d0e944
User Name <user@example.com>
2026-05-30 20:29:03 +0800

    fix: register PdfRendererPlugin in MainActivity - fixes 'not implemented on android' error

--- android/app/src/main/java/com/patent/hub/MainActivity.java
+++ android/app/src/main/java/com/patent/hub/MainActivity.java
@@ -1,5 +1,13 @@
 package com.patent.hub;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Register custom Capacitor plugins before super.onCreate()
+        registerPlugin(PdfRendererPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
